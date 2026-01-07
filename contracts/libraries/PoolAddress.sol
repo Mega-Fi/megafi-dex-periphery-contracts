a86129571585f51947e31c670bc5fadf32a037eb
anassohail99 <anas.sohailabs@gmail.com>
2026-01-08 01:41:21 +0500

    fix: Update POOL_INIT_CODE_HASH in PoolAddress library for consistency with latest deployment

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xa598dd2fba360510c5a8f02f44423a4468e902df5857dbce3ca162a43a3a31ff;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x15e5adda5b7156d36833ac17da4fd509ef49fc3fadb43ee594820f6dda94c7af;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
